fcfcf30cfec9bfed78cee794f222e0776cfe94d3
YeongWoo <rdd9223@gmail.com>
2022-05-22 16:58:04 +0900

    [STEP2] user 기본 모듈 생성
    - user 로그인, 이메일 인증, 회원가입
    - nodemailer를 이용한 이메일 인증

---
+++ src/users/users.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { User } from './entities/user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() dto: CreateUserDto) {
+    const { name, email, password } = dto;
+    return this.usersService.createUser(name, email, password);
+  }
+
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    const { email, password } = dto;
+
+    return await this.usersService.login(email, password);
+  }
+
+  @Get('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    const { signupVerifyToken } = dto;
+
+    return await this.usersService.verifyEmail(signupVerifyToken);
+  }
+
+  @Get('/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<User> {
+    return await this.usersService.getUserInfo(userId);
+  }
+}
